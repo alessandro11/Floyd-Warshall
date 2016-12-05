2d1d444f709573b302e00a3a78e5dd466267ae92
Alessandro Elias <ae11@c3sl.ufpr.br>
2016-12-04 23:48:19 -0200

    Test caminhos_min with floydwarshall.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- teste.c
+++ teste.c
@@ -79,19 +79,21 @@ int main(int argc, char* argv[]) {
     lista T, **T2;
 
     uint i, j;
-
-	lint **dist = (lint**)calloc(g->nvertices, sizeof(lint**));
-	for( i=0; i < g->nvertices; i++ ) {
-		dist[i] = (lint*)calloc(g->nvertices, sizeof(lint*));
-	}
-
-	distancias(dist, g, ' ');
-	print_mat_dist(dist, g->nvertices);
-
-	for( i=0; i < g->nvertices; i++ ) {
-		free(dist[i]);
-	}
-	free(dist);
+//
+//	lint **dist = (lint**)calloc(g->nvertices, sizeof(lint**));
+//	for( i=0; i < g->nvertices; i++ ) {
+//		dist[i] = (lint*)calloc(g->nvertices, sizeof(lint*));
+//	}
+//
+//	distancias(dist, g, 'd');
+//	print_mat_dist(dist, g->nvertices);
+//	distancias(dist, g, ' ');
+//	print_mat_dist(dist, g->nvertices);
+//
+//	for( i=0; i < g->nvertices; i++ ) {
+//		free(dist[i]);
+//	}
+//	free(dist);
 
 //	fprintf(stderr, "%d\n", direcionado(g));
 //	fprintf(stderr, "%ld\n", diametro(g));
@@ -104,22 +106,22 @@ int main(int argc, char* argv[]) {
 //    print_vbylista(T);
 //    destroi_lista(T, NULL);
 //
-//	T2 = (lista**)calloc(g->nvertices, sizeof(lista**));
-//	lista **p = T2;
-//	for( no n=primeiro_no(g->vertices); n; n=proximo_no(n) )
-//		*p++ = (lista*)calloc(g->nvertices, sizeof(lista*));
-//
-//	caminhos_minimos(T2, g);
-//	print_mat(T2, g);
-//
-//	p = T2;
-//	for( i=0; i < g->nvertices; i++  ) {
-//		for( j=0; j< g->nvertices; j++ ) {
-//			destroi_lista(T2[i][j], destroi_vertice);
-//		}
-//		free(*p++);
-//	}
-//	free(T2);
+	T2 = (lista**)calloc(g->nvertices, sizeof(lista**));
+	lista **p = T2;
+	for( no n=primeiro_no(g->vertices); n; n=proximo_no(n) )
+		*p++ = (lista*)calloc(g->nvertices, sizeof(lista*));
+
+	caminhos_minimos(T2, g, ' ');
+	print_mat(T2, g);
+
+	p = T2;
+	for( i=0; i < g->nvertices; i++  ) {
+		for( j=0; j< g->nvertices; j++ ) {
+			destroi_lista(T2[i][j], destroi_vertice);
+		}
+		free(*p++);
+	}
+	free(T2);
 
 
   printf("nome: %s\n", nome_grafo(g));
